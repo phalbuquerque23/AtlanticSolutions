2e29dbc28a8a6a5479d359895f7e595b2bd4411c
phalbuquerque23 <phalbuquerque.it@gmail.com>
2022-06-28 13:12:08 -0300

    projeto

---
+++ atlanticsolutions/src/main/java/br/com/atlanticsolutions/AtlanticsolutionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.atlanticsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlanticsolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlanticsolutionsApplication.class, args);
+	}
+
+}
---
+++ atlanticsolutions/src/test/java/br/com/atlanticsolutions/AtlanticsolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atlanticsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlanticsolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
